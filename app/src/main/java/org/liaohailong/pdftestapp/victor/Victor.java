7f908279180301885e675d8b41a027d479f0450e
liaohailong <liaohailong190@foxmail.com>
2018-04-30 19:22:38 +0800

    构思Victor请求框架蓝图。

---
+++ app/src/main/java/org/liaohailong/pdftestapp/victor/Victor.java
@@ -0,0 +1,17 @@
+package org.liaohailong.pdftestapp.victor;
+
+/**
+ * Describe as: 基于HttpUrlConnection封装的网络请求库
+ * 基本功能：
+ * 1，绑定Activity or Fragment的生命周期
+ * 2，手动移除网络队列中的任务（文件下载/上传除外）
+ * 3，上传文件、下载文件（多线程断点下载）
+ * 4，仿Volley的万箭齐发式请求（轻量级任务）
+ * 5，数据缓存，减少请求网络的频率，从而优化流量费用
+ * 6，自由修改全局统一的Http请求首部字段
+ * 7，全局统一的拦截器
+ * Created by LiaoHaiLong on 2018/4/30.
+ */
+
+public class Victor {
+}
